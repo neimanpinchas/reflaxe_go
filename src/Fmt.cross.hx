50137fa908c89b19b64aa51b7a6a9b4daf372690
neimanpinchas <neimanpinchas@gmail.com>
2024-12-18 14:56:15 +0200

    fmt overloads

--- src/Fmt.cross.hx
+++ src/Fmt.cross.hx
@@ -4,5 +4,6 @@ package;
 @:native("fmt")
 class Fmt {
     public extern static function Print(what:Dynamic):Void;
-    public extern static function Fprintf(stream:gostd.ComIface.Writer,whata:Dynamic,whatb:Dynamic):Void;
+    public overload extern static function Fprintf(stream:gostd.ComIface.Writer,whata:Dynamic,whatb:Dynamic):Void;
+    public overload extern static function Fprintf(stream:gostd.ComIface.Writer,whata:Dynamic):Void;
 }
